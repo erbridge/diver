0647f83822da9dda783ca8b525a5132419bb7f2a
erbridge <f@erbridge.co.uk>
2020-04-05 14:06:20 +0100

    Smooth the character movement

--- entities/Character/Character.gd
+++ entities/Character/Character.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
 var move_to_target = false
-var target = Vector2()
+var target = transform
 
 func _input(event):
   if event is InputEventScreenTouch:
     move_to_target = event.pressed
-    target = event.position
 
-  if event is InputEventScreenDrag:
-    target = event.position
+  if event is InputEventScreenTouch or event is InputEventScreenDrag:
+    target = Transform2D(event.position.angle_to_point(position), event.position)
 
 func _process(_delta):
   if move_to_target:
-    look_at(target)
-    position = target
+    transform = transform.interpolate_with(target, 0.1)
