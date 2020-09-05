000bf14539a91c910e597e47176ee4ae98fdbcfd
Michael Elbert <me@michael-elbert.com>
2020-09-05 17:55:01 +0200

    specks can only be collected once

--- entities/Speck/Speck.gd
+++ entities/Speck/Speck.gd
@@ -1,22 +1,9 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_Speck_area_entered(area):
 	print("collected a speck")
 	if (area.get_name() == "Character"):
 		get_tree().get_root().get_node("Main").get_plant_ui().start_fade_in()
-		self.hide()
+		for child in get_children():
+			child.queue_free()
+		queue_free()
