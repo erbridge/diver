c608d337af930756495a6c787719cd3c0da7ebf9
Michael Elbert <me@michael-elbert.com>
2020-09-06 19:08:22 +0200

    specks are now at a random position

--- entities/Speck/Speck.gd
+++ entities/Speck/Speck.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var rnd = RandomNumberGenerator.new()
+
 func _on_Speck_area_entered(area):
 	print("collected a speck")
 	if (area.get_name() == "Character"):
@@ -9,3 +11,9 @@ func _on_Speck_area_entered(area):
 		for child in get_children():
 			child.queue_free()
 		queue_free()
+
+func _ready() -> void:
+	rnd.seed = hash(OS.get_system_time_msecs())
+	var randX = rnd.randf_range(0, 3000) - 1500
+	var randY = rnd.randf_range(0, 150) + 1800
+	position = Vector2(randX, randY)
