ad8f44ddea6a366da1301456ae99592455ce143b
Michael Elbert <me@michael-elbert.com>
2020-09-06 18:45:56 +0200

    saving + loading

--- entities/Plants/Plant.gd
+++ entities/Plants/Plant.gd
@@ -18,3 +18,12 @@ func _process(delta) -> void:
 		_is_growing = false
 		
 	scale = Vector2.ONE * _accumulated_scale
+
+func save() -> Dictionary:
+	var dict = {
+		"filename" : get_filename(),
+		"parent" : get_parent().get_path(),
+		"pos_x" : position.x, # Vector2 is not supported by JSON
+		"pos_y" : position.y
+	}
+	return dict
