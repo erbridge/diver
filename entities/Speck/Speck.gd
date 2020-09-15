ae56b4804be29e7ee7171bc893a800e2e0304318
Michael Elbert <me@michael-elbert.com>
2020-09-15 14:56:38 +0200

    speck now moves in a messy sin+cos curve. predictable, but confusing enough that it feels playful hopefully?

--- entities/Speck/Speck.gd
+++ entities/Speck/Speck.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 var rnd = RandomNumberGenerator.new()
+var time_spent = 0.0
+var center_pos = Vector2.ZERO
 
 func _on_Speck_area_entered(area):
 	print("collected a speck")
@@ -14,6 +16,13 @@ func _on_Speck_area_entered(area):
 
 func _ready() -> void:
 	rnd.seed = hash(OS.get_system_time_msecs())
-	var randX = rnd.randf_range(0, 3000) - 1500
-	var randY = rnd.randf_range(0, 150) + 1800
-	position = Vector2(randX, randY)
+	var randX = 0.0##rnd.randf_range(0, 3000) - 1500
+	var randY = rnd.randf_range(0, 150) + 1400
+	center_pos = Vector2(randX, randY)
+
+func _process(delta) -> void:
+	time_spent += delta
+	var xWidth = 350.0 + sin(time_spent * 0.8) * 50.0
+	var yWidth = 180.0 + sin(time_spent) * 30.0
+	position.x = center_pos.x + sin(time_spent * 1.5) * xWidth
+	position.y = center_pos.y + cos(time_spent * 0.5) * yWidth + sin(time_spent) * 50.0
